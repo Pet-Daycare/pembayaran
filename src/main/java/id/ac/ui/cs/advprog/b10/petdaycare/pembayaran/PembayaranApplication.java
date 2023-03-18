678d87ac42fdc80e8ada0027f99860b5a1de553d
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-03-18 21:10:14 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/b10/petdaycare/pembayaran/PembayaranApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b10.petdaycare.pembayaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PembayaranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PembayaranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/b10/petdaycare/pembayaran/PembayaranApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b10.petdaycare.pembayaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PembayaranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
